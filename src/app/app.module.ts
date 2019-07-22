52178833e7afab22962a4d8c750affe8911e6666
danieltorres31 <danieltorres31@outlook.com>
2019-07-22 20:45:07 -0300

    chore: Importa AnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { HomeModule } from './components/home/home.module';
 import { CarouselInitialModule } from './components/carousel-initial/carousel-initial.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,6 +14,7 @@ import { TopMenuModule } from './components/top-menu/top-menu.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     TopMenuModule,
