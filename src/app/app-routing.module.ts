2112b7ce8922c7b7e6ae55e47884667289ff2103
danieltorres31 <danieltorres31@outlook.com>
2019-08-21 20:08:17 -0300

    refactor: Remove comentario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { CarouselInitialComponent } from './components/carousel-initial/carousel-initial.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -7,8 +6,6 @@ import { AboutUsComponent } from './components/about-us/about-us.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-
-  //Rota Quem Somos
   {path: 'quemsomos', component: AboutUsComponent},
 ];
 
