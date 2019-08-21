5567b1cdabd2e36fd52163c606393aa1cddaeff5
danieltorres31 <danieltorres31@outlook.com>
2019-08-21 20:07:04 -0300

    feat: Implementa AnimalsView

---
+++ src/app/components/animals-view/animals-view.module.ts
@@ -0,0 +1,19 @@
+import { AnimalCardModule } from './../animal-card/animal-card.module';
+import { AnimalService } from './../../shared/animal/animal.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AnimalsViewComponent } from './animals-view.component';
+import { DomSanitizer } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [AnimalsViewComponent],
+  imports: [
+    CommonModule,
+    AnimalCardModule,
+  ],
+  providers: [AnimalService],
+  exports: [AnimalsViewComponent]
+})
+export class AnimalsViewModule { }
