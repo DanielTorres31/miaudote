fdbae2c23444c303fa2d57b54ccec567a74921ed
danieltorres31 <danieltorres31@outlook.com>
2019-09-04 20:14:47 -0300

    feat: cria filtro basico

--- src/app/components/side-filter/side-filter.module.ts
+++ src/app/components/side-filter/side-filter.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideFilterComponent } from './side-filter.component';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 
 @NgModule({
   declarations: [SideFilterComponent],
   imports: [
     CommonModule,
+    ButtonsModule,
   ],
   exports: [SideFilterComponent]
 })
