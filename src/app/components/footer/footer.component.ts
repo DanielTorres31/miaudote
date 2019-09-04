e6efcbdd8d464b0e67ba4efcb819bc09ff90c81e
Henrique Batista <henrique.batista@epoca.dc>
2019-09-04 17:49:35 -0300

    Feat: Ajusta footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -10,18 +10,19 @@ export class FooterComponent implements OnInit {
   
   constructor() { }
     items = [
-      {icon: 'fa fa-facebook', url: 'https://www.facebook.com/'},
-      {icon: 'fa fa-twitter', url: '#'},
-      {icon: 'fa fa-instagram', url: '#'},
-      {icon: 'fa fa-google-plus', url: '#'},
-      {icon: 'fa fa-envelope', url: '#'}
+      {id:'social-fb', icon: 'fa fa-facebook fa-3x social', url: 'https://www.facebook.com'},
+      {id:'social-tw', icon: 'fa fa-twitter fa-3x social', url: '#'},
+      {id:'social-in', icon: 'fa fa-instagram fa-3x social', url: 'https://www.instagram.com/miaudote.newton/'},
+      {id:'social-gp', icon: 'fa fa-google-plus fa-3x social', url: '#'},
+      {id:'social-em', icon: 'fa fa-envelope fa-3x social', url: '#'}
     ];
 
   ngOnInit() {
+
   }
 
   defineFooter(){
-    if(window.innerWidth < 900){
+    if(window.innerWidth <= 900){
       return "footerMobile"
     }else
       return "footer"
