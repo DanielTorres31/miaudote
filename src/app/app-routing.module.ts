b5da6c2e3fc118319010ebf7808039aa6770afec
danieltorres31 <danieltorres31@outlook.com>
2019-10-12 19:59:31 -0300

    feat: Configura rota de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { HowToAdoptComponent } from './components/how-to-adopt/how-to-adopt.component';
 import { AnimalDetailComponent } from './components/animal-detail/animal-detail.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'quemsomos', component: AboutUsComponent },
   { path: 'comoadotar', component: HowToAdoptComponent },
-  { path: 'detalhes/:id', component: AnimalDetailComponent }
+  { path: 'detalhes/:id', component: AnimalDetailComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
