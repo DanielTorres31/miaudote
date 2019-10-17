e0f046b9b803785e9d2acb14c5626453d2b3a6e9
danieltorres31 <danieltorres31@outlook.com>
2019-10-16 21:03:02 -0300

    feat: Add rota HomeAdmin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { AboutUsComponent } from './components/about-us/about-us.component';
 import { HowToAdoptComponent } from './components/how-to-adopt/how-to-adopt.component';
 import { AnimalDetailComponent } from './components/animal-detail/animal-detail.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeAdminComponent } from './components/home-admin/home-admin.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'quemsomos', component: AboutUsComponent },
   { path: 'comoadotar', component: HowToAdoptComponent },
   { path: 'detalhes/:id', component: AnimalDetailComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: HomeAdminComponent }
 ];
 
 @NgModule({
