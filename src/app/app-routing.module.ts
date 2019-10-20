77265edec5ca86af8c9551e24778121aa6f7dac7
danieltorres31 <danieltorres31@outlook.com>
2019-10-20 08:40:34 -0300

    feat: Add guarda de rotas app.routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,17 @@ import { HowToAdoptComponent } from './components/how-to-adopt/how-to-adopt.comp
 import { AnimalDetailComponent } from './components/animal-detail/animal-detail.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeAdminComponent } from './components/home-admin/home-admin.component';
+import { NormalPageGuard } from './guard/normal-page.guard';
+import { LoginPageGuard } from './guard/login-page.guard';
+import { AdminPageGuard } from './guard/admin-page.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'quemsomos', component: AboutUsComponent },
-  { path: 'comoadotar', component: HowToAdoptComponent },
-  { path: 'detalhes/:id', component: AnimalDetailComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'admin', component: HomeAdminComponent }
+  { path: '', component: HomeComponent, canActivate: [NormalPageGuard] },
+  { path: 'quemsomos', component: AboutUsComponent, canActivate: [NormalPageGuard] },
+  { path: 'comoadotar', component: HowToAdoptComponent, canActivate: [NormalPageGuard] },
+  { path: 'detalhes/:id', component: AnimalDetailComponent, canActivate: [NormalPageGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [LoginPageGuard] },
+  { path: 'admin', component: HomeAdminComponent, canActivate: [AdminPageGuard] }
 ];
 
 @NgModule({
