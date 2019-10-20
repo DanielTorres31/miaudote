98e40ba9904a3fa15ed8de0f20a2efcd6b7f49b2
danieltorres31 <danieltorres31@outlook.com>
2019-10-20 11:58:08 -0300

    feat: Remove codigo inutil home-admin

--- src/app/components/home-admin/home-admin.module.ts
+++ src/app/components/home-admin/home-admin.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeAdminComponent } from './home-admin.component';
-import { AdminMenuModule } from '../admin-menu/admin-menu.module';
-import { TableModule } from 'src/app/html-components/table/table.module';
 
 @NgModule({
   declarations: [HomeAdminComponent],
   imports: [
     CommonModule,
-    AdminMenuModule,
-    TableModule,
   ],
   exports: [HomeAdminComponent]
 })
