f84cd65c27dec42d11d53754c36d14c0ceb354ec
danieltorres31 <danieltorres31@outlook.com>
2019-10-20 11:57:42 -0300

    feat: Add efeito hover na tabela e evento de click

--- src/app/html-components/table/table.component.ts
+++ src/app/html-components/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Header } from './shared/header.model';
 
 @Component({
@@ -11,8 +11,14 @@ export class TableComponent implements OnInit {
   @Input() list: any[];
   @Input() headers: Header[];
 
+  @Output() clickedElement = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() { }
+
+  clickElement(element) {
+    this.clickedElement.emit(element);
+  }
 
 }
