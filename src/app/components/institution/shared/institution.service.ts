090de828db5dfd74e5a650bec2a8437f5f8b2490
danieltorres31 <danieltorres31@outlook.com>
2019-10-22 22:13:04 -0300

    feat: Add novos metodos InstitutionService

--- src/app/components/institution/shared/institution.service.ts
+++ src/app/components/institution/shared/institution.service.ts
@@ -1,3 +1,4 @@
+import { Institution } from './institution.model';
 import { environment } from './../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -11,6 +12,18 @@ export class InstitutionService {
 
     constructor(private http: HttpClient) { }
     
+    create(institution: Institution) {
+        return this.http.post(this.urlService, institution);
+    }
+
+    update(institution: Institution) {
+        return this.http.put(this.urlService, institution);
+    }
+
+    delete(id: Number) {
+        return this.http.delete(`${this.urlService}/${id}`);
+    }
+
     findAll() {
         return this.http.get(this.urlService);
     }
