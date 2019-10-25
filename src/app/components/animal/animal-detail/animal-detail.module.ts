e5656ff8e1f0f273c6ec21b1a4306526f79b3541
Henrique Gomes <henriquecebrac37@gmail.com>
2019-10-24 22:56:38 -0300

    feat: ajusta animal detail

--- src/app/components/animal/animal-detail/animal-detail.module.ts
+++ src/app/components/animal/animal-detail/animal-detail.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnimalDetailComponent } from './animal-detail.component';
 import { AnimalService } from 'src/app/components/animal/shared/animal.service';
+import {CardModule} from 'primeng/card';
 
 
 
 @NgModule({
   declarations: [AnimalDetailComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CardModule
   ],
   exports: [AnimalDetailComponent],
   providers: [AnimalService]
