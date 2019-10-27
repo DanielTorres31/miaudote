288cf4d57da2a5b79d047b4329dd014dcb74d322
danieltorres31 <danieltorres31@outlook.com>
2019-10-27 16:21:46 -0300

    feat: Add telefone da instituição - detalhes animal

--- src/app/components/animal/animal-detail/animal-detail.component.ts
+++ src/app/components/animal/animal-detail/animal-detail.component.ts
@@ -1,8 +1,10 @@
+import { InstitutionService } from './../../institution/shared/institution.service';
 import { Component, OnInit, ɵConsole } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AnimalService } from 'src/app/components/animal/shared/animal.service';
 import { Animal } from 'src/app/components/animal/shared/animal.model';
 import { AnimalUtils } from './../shared/animal-utils';
+import { Institution } from '../../institution/shared/institution.model';
 
 @Component({
   selector: 'app-animal-detail',
@@ -13,15 +15,28 @@ export class AnimalDetailComponent implements OnInit {
 
   animal: Animal = new Animal();
 
-  constructor(private route: ActivatedRoute, private animalService: AnimalService) { }
+  institution: Institution = new Institution();
+
+  constructor(private route: ActivatedRoute, private animalService: AnimalService, private institutionService: InstitutionService) { }
 
   ngOnInit() {
     this.route.params.subscribe(param => {
 
-      this.animalService.findById(param['id'], true).subscribe((response: any) => {
-        this.animal = AnimalUtils.enrichmentAnimal(response.data);
-      });
+      this.findAnimal(param['id']);
 
+    });
+  }
+
+  findAnimal(idAnimal) {
+    this.animalService.findById(idAnimal, true).subscribe((response: any) => {
+      this.animal = AnimalUtils.enrichmentAnimal(response.data);
+      this.findInstitution(this.animal.INSTITUICAO_COD_INSTITUICAO);
+    });
+  }
+
+  findInstitution(idInstitution) {
+    this.institutionService.findById(idInstitution).subscribe((response: any) => {
+      this.institution = response.data;
     });
   }
 
