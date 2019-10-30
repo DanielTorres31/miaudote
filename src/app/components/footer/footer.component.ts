03a6ff22127a1f0360d3b9a9a99a97e10273c8fa
Henrique Batista <henrique.batista@epoca.dc>
2019-10-30 15:21:47 -0300

    Feat: Padroniza fontes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -11,11 +11,7 @@ export class FooterComponent implements OnInit {
   
   constructor() { }
     items = [
-      {id:'social-fb', icon: 'fa fa-facebook fa-3x social', url: 'https://www.facebook.com'},
-      {id:'social-tw', icon: 'fa fa-twitter fa-3x social', url: '#'},
       {id:'social-in', icon: 'fa fa-instagram fa-3x social', url: 'https://www.instagram.com/miaudote.newton/'},
-      {id:'social-gp', icon: 'fa fa-google-plus fa-3x social', url: '#'},
-      {id:'social-em', icon: 'fa fa-envelope fa-3x social', url: '#'}
     ];
 
   ngOnInit() {
