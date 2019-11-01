880844c1cbdd8b7b21552be40666ed814ebfd904
danieltorres31 <danieltorres31@outlook.com>
2019-10-31 21:11:01 -0300

    feat: Busca animais na home e passa para components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { SideFilter } from './../side-filter/shared/side-filter.model';
+import { AnimalService } from './../animal/shared/animal.service';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Animal } from '../animal/shared/animal.model';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+ animals: Animal[] = new Array<Animal>();
+
+  constructor(private animalService: AnimalService) { }
 
   ngOnInit() {
+    this.getAnimals();
+  }
+
+  getAnimals() {
+    this.animalService.findAll(true).subscribe((response: any) => {
+      this.animals = response.data;
+    });
+  }
+
+  changeFilter(event: any) {
+    this.animalService.filterAnimals(event.filter, true).subscribe((response: any) => {
+      this.animals = response.data;
+    });
   }
 
 }
